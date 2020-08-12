c52091173ba61a94bec2e55543c6acaf9e6b3b6f
Aslan <baristan1@rambler.ru>
2020-08-12 14:41:27 +0300

    test1

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+sudo /etc/init.d/gunicorn restart
+
