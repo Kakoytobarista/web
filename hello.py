2309a43452ec3c1bac4efae78648a08d8b7b1c2a
Aslan <baristan1@rambler.ru>
2020-08-12 17:14:09 +0300

    test4

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-def app(environ, start_response):
-    """Simplest possible application object"""
-    data = b'Hello, World!\n'
+def wsgi_application(environ, start_response):
     status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+    headers = [
+        ('Content-Type', 'text/plain')
     ]
-    start_response(status, response_headers)
-    return iter([data])
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers )
+    return [ body ]
+
